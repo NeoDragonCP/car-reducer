d7491b4bb61f8d96c06bbcd849ed8649b41a490f
NeoDragon <mcvickerstephen@gmail.com>
2020-08-25 22:15:22 +0100

    Updates

---
+++ precache-manifest.747927a1d34cb4cc0f0c59010a7a222c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc386a92156dbf0df094644f3954e701",
+    "url": "/car-reducer/index.html"
+  },
+  {
+    "revision": "607cddc148eabcb070c4",
+    "url": "/car-reducer/static/css/main.39a55011.chunk.css"
+  },
+  {
+    "revision": "6f6eb4eb774d8e2f5f65",
+    "url": "/car-reducer/static/js/2.307dbb3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/car-reducer/static/js/2.307dbb3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607cddc148eabcb070c4",
+    "url": "/car-reducer/static/js/main.55411fcd.chunk.js"
+  },
+  {
+    "revision": "fec131a710d47b03a7ca",
+    "url": "/car-reducer/static/js/runtime-main.c2c9b34d.js"
+  }
+]);
